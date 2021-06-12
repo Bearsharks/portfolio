8524671b1ca4bc66226c3b371aee8d2eb28b40bb
bearsharks <bearsharks02@gmail.com>
2021-06-12 23:09:09 +0900

    뷰포트에 잡힌 list가 서서히 나타나는 애니메이션 구현

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ function Home() {
     const wrapperRef = useRef(null);
     const introRef = useRef(null);
     const articleRef = useRef(null);
-    const testRef = useRef(null);
+    const articleFrameRef = useRef(null);
     let lastReq = null;
     let prevScrollTop = 0;
     let isViewportChagned = false;
@@ -24,15 +24,43 @@ function Home() {
         lastReq = requestAnimationFrame(renderFrame.bind(this));
         if (!isViewportChagned) return;
         isViewportChagned = false;
+        let frameProgress = 0;
         if (wrapperRef.current.scrollTop > introRef.current.clientHeight) {
             let articleScrollTop = wrapperRef.current.scrollTop - introRef.current.clientHeight;
             let viewportSize = wrapperRef.current.clientHeight;
             //article.Height == 전체 높이 - article.offsetTop
             let maxScrollTop = (articleRef.current.clientHeight - viewportSize);
-            let frameProgress = articleScrollTop / maxScrollTop;
-            testRef.current.style.transform = `translate(${-frameProgress * (testRef.current.clientWidth - articleRef.current.clientWidth)}px, ${articleScrollTop}px)`;
+            frameProgress = Math.min(0.9999, articleScrollTop / maxScrollTop);
+            let curTop = Math.min(maxScrollTop, articleScrollTop);
+            articleFrameRef.current.style.transform = `translate(${-frameProgress * (articleFrameRef.current.clientWidth - articleRef.current.clientWidth)}px, ${curTop}px)`;
         } else {
-            testRef.current.style.transform = 'translate(0,0)';
+            articleFrameRef.current.style.transform = 'translate(0,0)';
+        }
+        //10% 이상 보이면 스타일 바꿔줌 
+        let childCnt = articleFrameRef.current.children[0].childElementCount;
+        let eleSize = 1 / (childCnt - 1);
+        let first = Math.floor(frameProgress / eleSize);
+        let firstShownSizePer = (eleSize - (frameProgress % eleSize)) / eleSize;
+
+        if (firstShownSizePer > 0.15) {
+            articleFrameRef.current.children[0].children[first].style.opacity = 1;
+            articleFrameRef.current.children[0].children[first].style.transform = `translateY(0)`;
+        }
+        else {
+            articleFrameRef.current.children[0].children[first].style.opacity = "";
+            articleFrameRef.current.children[0].children[first].style.transform = "";
+        }
+
+        let second = Math.floor((frameProgress + eleSize) / eleSize);
+        let secondShownSize = (frameProgress % eleSize) / eleSize;
+
+        if (secondShownSize > 0.15) {
+            articleFrameRef.current.children[0].children[second].style.opacity = 1;
+            articleFrameRef.current.children[0].children[second].style.transform = `translateY(0)`;
+        }
+        else {
+            articleFrameRef.current.children[0].children[second].style.opacity = "";
+            articleFrameRef.current.children[0].children[second].style.transform = "";
         }
     }
 
@@ -59,21 +87,22 @@ function Home() {
             </div>
             <div
                 ref={articleRef}
-                className="flex-wrapper__article"
-
+                className="flex-wrapper-article"
             >
-                <div ref={testRef} className="test">
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
-                    detail detail detail detail detail detail detail detail
+                <div ref={articleFrameRef} className="article-frame">
+                    <ul>
+                        <li>a</li>
+                        <li>b</li>
+                        <li>c</li>
+                        <li>d</li>
+                        <li>e</li>
+                        <li>f</li>
+                        <li>g</li>
+                    </ul>
                 </div>
+            </div>
+            <div className="test">
+                test
             </div>
         </div>
 
